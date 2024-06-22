16704147803683225e7a6695819828267a5834b9
Caio Antunes de Oliveira <110792539+Caio0liveira@users.noreply.github.com>
2024-06-22 16:43:39 -0300

    Upando porftfólio

---
+++ Meteora/src/script.js
@@ -0,0 +1,7 @@
+const btnMenu = document.getElementById("btn-menu")
+
+function toggleMenu(){
+    const nav = document.getElementById("nav")
+    nav.classList.toggle("active")
+}
+btnMenu.addEventListener("click", toggleMenu)
